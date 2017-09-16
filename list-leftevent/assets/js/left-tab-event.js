f4b9b6943e57b66adecd9ba28b5cfa1402a365b6
caimoxuan <cmx18725122527>
2017-09-16 09:41:29 +0800

    coding

--- list-leftevent/assets/js/left-tab-event.js
+++ list-leftevent/assets/js/left-tab-event.js
@@ -4,7 +4,7 @@
 
   var __CONTENT__ = {
       messagelist : new Array(),
-      rolenow : new Role("1", "main", "img/1.png", "online");
+      rolenow : new Role("1", "main", "img/1.png", "online")
   }
 
   function Message(roleid, messagehtml){
@@ -17,33 +17,58 @@
     this.rolename = rolename;
     this.headpath = headpath;
     this.onlinestate = onlinestate;
-    this.html = '<li data-role-list="common" ><a href = "#"><img src = "'+headpath+'" class = "'+onlinestate+'">'+rolename+'</a><a href = "#" onclick = "removeRole()"><span>X</span></a></li>';
+    this.html = '<li class="role-list" id = "'+roleid+'"><a href="#" class="ui-link"><img src="'+headpath+'"></a></li>';
     this.message = "";
   }
 
   $.fn[options] = function(fileds){
-
+	
   }
 
-  window.addRole = function(){
-    var role = new Role("123","cmx","img/1.png","online");
-    $.each(__CONTENT__.messagelist, function(index){
-      if(__CONTENT__.messagelist[index].roleid === role.roleid){
-        console.log("请不要重复点击");
-      }else{
-        
-        $("#right").html(role.html);
-        __CONTENT__.messagelist.push(role);
-      }
-    });
-
+  window.addRole = function(role){
+	var role = new Role(role.roleid,role.rolename, role.headpath, role.state);
+	var exist = contain(role.roleid);
+	
+	if(exist != -1){
+		console.log("请不要重复点击");
+	}else{
+		var roleUl = $("#role-container").find('ul');
+		var rolist = roleUl.find('li');
+		rolist.css({"border-right":"none"});
+		var ulHtml = $("#right").html();
+		$("#right").html(ulhtml+role.html);
+		__CONTENT__.messagelist.push(role);
+	}
 
   }
+  
+  window.clickRole(e){
+	  var roleid = e.attr('id');
+	  var roleUl = $("#role-container").find('ul');
+	  var rolist = roleUl.find('li');
+	  rolist.css({"border-right":"none"});
+	  var ulHtml = $("#right").html();
+  }
 
-  window.removeRole = function(){
-
+  window.removeRole = function(roleid){
+	
+  }
+  
+  function contain(roleid){
+	  var roleIdList = new Array();
+	  $.each(__CONTENT__.messagelist, function(index){
+		  roleIdList.push(__CONTENT__.messagelist[index].roleid);
+	  });
+	  return roleIdList.indexOf(roleid);
   }
 
 
 
 },"leftTabEvent");
+
+/*需要参数：
+1.显示消息的ul 的id
+
+
+
+*/
