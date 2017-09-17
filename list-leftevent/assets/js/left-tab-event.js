8fcc0a27bb122df35950120bf14712dc1011624c
caimoxuan <cmx18725122527>
2017-09-17 22:42:41 +0800

    list-ledtevent
    message can be resolve but can not be save

--- list-leftevent/assets/js/left-tab-event.js
+++ list-leftevent/assets/js/left-tab-event.js
@@ -3,8 +3,8 @@
 })(window, function($, options){
 
   var __CONTENT__ = {
-      messagelist : new Array(),
-      rolenow : new Role("1", "main", "img/1.png", "online")
+      rolelist : new Array(new Role(0, "main", "img/1.png", "online")),
+      rolenow : new Role(0, "main", "img/1.png", "online")
   }
 
   function Message(roleid, messagehtml){
@@ -17,7 +17,7 @@
     this.rolename = rolename;
     this.headpath = headpath;
     this.onlinestate = onlinestate;
-    this.html = '<li class="role-list" id = "'+roleid+'"><a href="#" class="ui-link"><img src="'+headpath+'">'+rolename+'</a></li>';
+    this.html = '<li class="role-list" data-chater = "'+roleid+'"><a href="#" class="ui-link"><img src="'+headpath+'"></a></li>';
     this.message = "";
   }
 
@@ -27,39 +27,65 @@
 
   window.addRole = function(role){
 	var role = new Role(role.roleid,role.rolename, role.headpath, role.state);
-	var exist = contain(role.roleid);
+	var exist = hasChatRole(role.roleid);
 	
-	if(exist != -1){
+	if(exist){
 		console.log("请不要重复点击");
 	}else{
 		var roleUl = $("#role-container").find('ul');
-		var rolist = roleUl.find('li');
-		rolist.css({"border-right":"none"});
-		var ulHtml = $("#right").html();
-		$("#right").html(ulHtml+role.html);
-		__CONTENT__.messagelist.push(role);
+		var ulHtml = roleUl.html();
+		roleUl.html(ulHtml+role.html);
+		__CONTENT__.rolelist.push(role);
+		
+		var allli = $("#role-container").find('ul').find('li');
+		allli.on('click', function(){clickRole(this);});
+		var li = $("#role-container").find('ul').find('li[data-chater='+role.roleid+']');
+		clickRole(li);
 	}
 
   }
   
   window.clickRole = function(e){
-	  var roleid = e.attr('id');
+	  var roleid = $(e).data('chater');
+	  
 	  var roleUl = $("#role-container").find('ul');
 	  var rolist = roleUl.find('li');
 	  rolist.css({"border-right":"none"});
-	  var ulHtml = $("#right").html();
+	  var messageHtml = $("#responseText").html();
+	  var index = findRole(__CONTENT__.rolenow.roleid);
+	  __CONTENT__.rolelist[index].message=messageHtml;
+	  var roleindex = findRole(roleid);
+	  __CONTENT__.rolenow = __CONTENT__.rolelist[roleindex];
+	  $("#responseText").html(__CONTENT__.rolelist[roleindex].message);
   }
 
   window.removeRole = function(roleid){
-	
+	var li = $("#role-container").find('ul').find('li[data-chater='+roleid+']');
+	li.remove();
+  }
+  
+  function hasChatRole(roleid){
+	  var li = $("#role-container").find('ul').find('li[data-chater='+roleid+']');
+	  return li.length > 0;
   }
   
   function contain(roleid){
 	  var roleIdList = new Array();
-	  $.each(__CONTENT__.messagelist, function(index){
-		  roleIdList.push(__CONTENT__.messagelist[index].roleid);
+	  $.each(__CONTENT__.rolelist, function(index){
+		  roleIdList.push(__CONTENT__.rolelist[index].roleid);
 	  });
 	  return roleIdList.indexOf(roleid);
+  }
+  
+  function findRole(roleid){
+	  var i;
+	  $.each(__CONTENT__.rolelist, function(index){
+		 if(__CONTENT__.rolelist[index].roleid === roleid){
+			 i = index;
+			 return false;
+		 } 
+	  });
+	  return i;
   }
 
 
